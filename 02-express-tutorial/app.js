be827d4a3e353a01386ea2824efeb95ad46ac06d
Ahmed Hisham <ahmedhisham1928@gmail.com>
2025-02-14 14:42:02 +0100

    ..

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,27 @@
-console.log('Express Tutorial')
+const http = require("http");
+const { readFileSync } = require("fs");
+
+// get all files
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
+
+const server = http
+  .createServer((req, res) => {
+    const url = req.url;
+    if (url === "/") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write(homePage);
+      res.end();
+    } else if (url === "/about") {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.write("<h1>About Page</h1>");
+      res.end();
+    } else {
+      res.writeHead(404, { "content-type": "text/html" });
+      res.write("<h1>page not found</h1>");
+      res.end();
+    }
+  })
+  .listen(3000);
